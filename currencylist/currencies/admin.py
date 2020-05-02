52cc872865e7315b062b6ada2b1cb618691f9aa8
root <root@DESKTOP-17CC21M.localdomain>
2020-05-02 19:19:19 +0300

    Register database models

--- currencylist/currencies/admin.py
+++ currencylist/currencies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cryptocurrency, Currency
+
+
+@admin.register(Cryptocurrency)
+class CryptocurrencyModelAdmin(admin.ModelAdmin):
+    """ Cryptocurrency model admin """
+    list_display = ('abbreviation', 'name', 'price', 'change')
+
+
+@admin.register(Currency)
+class CurrencyModelAdmin(admin.ModelAdmin):
+    """ Currency model admin """
+    list_display = ('abbreviation', 'name', 'price', 'unit')
