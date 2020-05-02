e29141aebe72463ee61fb55906e154921d837566
root <root@DESKTOP-17CC21M.localdomain>
2020-05-02 19:30:01 +0300

    Edit route for application

--- currencylist/currencylist/urls.py
+++ currencylist/currencylist/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('currencylist/', include('currencies.urls'))
+    path('', include('currencies.urls'))
 ]
