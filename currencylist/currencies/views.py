007bc985a8353a1bc82fea1f45b5eec1de86ab9a
root <root@DESKTOP-17CC21M.localdomain>
2020-05-02 19:19:29 +0300

    Create views

--- currencylist/currencies/views.py
+++ currencylist/currencies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Cryptocurrency, Currency
+
+
+class CryptocurrencyListView(ListView):
+    """ Cryptocurrency list view """
+    model = Cryptocurrency
+    template_name = 'currencies/cryptocurrencies.html'
+    context_object_name = 'cryptocurrencies'
+
+
+class CurrencyListView(ListView):
+    """ Currency list view """
+    model = Currency
+    template_name = 'currencies/currencies.html'
+    context_object_name = 'currencies'
